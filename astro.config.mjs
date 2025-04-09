afb170cf1d81de2205c4fc7f65e8fe11a85c465f
acbuie <aidancbuie@gmail.com>
2025-04-08 20:25:36 -0600

    feat: Astro Github actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://acbuie.github.io",
+
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()],
   },
 });
-
